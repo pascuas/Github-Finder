b05bc566a1c118ae24f84212c4d37f615511517a
Stanley Pascua <stanleypascua@Stanleys-MacBook-Pro.local>
2020-06-01 15:24:26 -0700

    added conditional in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const name = 'John Doe'
+    const loading = false;
+    const showName = true;
+
     return (
       <div className="App">
-        <h1>Hello from react</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
