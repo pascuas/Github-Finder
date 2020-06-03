bbf9406fa082885ad60ef0d5583a40de605714dd
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-06-03 15:18:14 -0700

    added repos and repoitem component

--- app/src/App.js
+++ app/src/App.js
@@ -13,8 +13,9 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
-    alert: null
+    alert: null,
   };
 
   // search github users
@@ -35,6 +36,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   } 
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos??per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  } 
+
   // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -47,7 +57,7 @@ class App extends Component {
 
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
 
     return (
@@ -65,7 +75,7 @@ class App extends Component {
             )} />
             <Route exact path='/about' component={About} />
             <Route exact path='/user/:login' render={props => (
-              <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+              <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />
             )} />
           </Switch>
         </div>
